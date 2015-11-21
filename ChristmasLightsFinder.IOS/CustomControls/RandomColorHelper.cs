faaf35a1450372519252eb0579dfeaad9de3d782
jeffbmiller <jeffmiller11@gmail.com>
2015-11-20 19:46:34 -0600

    Better Random Colors

--- ChristmasLightsFinder.IOS/CustomControls/RandomColorHelper.cs
+++ ChristmasLightsFinder.IOS/CustomControls/RandomColorHelper.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using UIKit;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChristmasLightsFinder.IOS
 {
 	public static class RandomColorHelper
 	{
+
+		private static readonly Random random = new Random();
+		private static readonly object syncLock = new object();
 		public static UIColor GetRandomColor()
 		{
-			var rnd = new Random();
-			var colorIndex = rnd.Next(0, 3);
+			var colors = new List<UIColor> (){UIColor.Blue, UIColor.Red, UIColor.Green,  UIColor.Purple, UIColor.Orange};
 
-			var colors = new List<UIColor> (){UIColor.Blue, UIColor.Red, UIColor.Green,  UIColor.Purple};
-
-			return colors[colorIndex];
+			lock(syncLock) { // synchronize
+				return colors.ElementAt(random.Next(0, 4));
+			}
 		}
 	}
 }
