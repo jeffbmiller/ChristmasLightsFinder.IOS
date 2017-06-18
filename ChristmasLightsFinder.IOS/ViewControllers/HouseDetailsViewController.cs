554a957cc60c6d562322656249e5e99d8756dc9e
jeffbmiller <jeffmiller11@hotmail.com>
2017-06-18 09:10:37 -0500

    Firebase Storage downloading

--- ChristmasLightsFinder.IOS/ViewControllers/HouseDetailsViewController.cs
+++ ChristmasLightsFinder.IOS/ViewControllers/HouseDetailsViewController.cs
@@ -43,16 +43,20 @@ namespace ChristmasLightsFinder.IOS
 
 			BindData ();
 
-            var rootNode = Firebase.Storage.Storage.DefaultInstance.GetRootReference();
-			// Create a reference to the file you want to download
-            var imagesNode = rootNode.GetChild($"images/{Annotation.House.Id}.jpg");
+
+			this.activityIndicator.Hidden = false;
+			this.activityIndicator.StartAnimating ();
+			var imageRef = Firebase.Storage.Storage.DefaultInstance.GetReferenceFromUrl(Annotation.House.ImagePath);
 
 			// Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-			imagesNode.GetData(1 * 1024 * 1024, (data, error) =>
+			imageRef.GetData(1 * 1024 * 1024, (data, error) =>
 			{
+                this.activityIndicator.Hidden = true;
+				this.activityIndicator.StopAnimating();
 				if (error != null)
 				{
 					// Uh-oh, an error occurred!
+
 				}
 				else
 				{
@@ -60,51 +64,10 @@ namespace ChristmasLightsFinder.IOS
 					var image = UIImage.LoadFromData(data);
                     this.houseImage.Image = image;
 				}
+
 			});
 
-			//if (Annotation.House.ImagePath != null) {
-				//try {
-					//this.activityIndicator.Hidden = false;
-					//this.activityIndicator.StartAnimating ();
-
-
-					//UIImage image;
-
-				//	//Check Cache First
-				//	var houseImageCache = await imageCacheRepo.GetHouseImagesFor(Annotation.House.Id);
-				//	if (houseImageCache == null)
-				//	{
-				//		//If Not in Cache set cache;
-				//		var byteArray = await new HttpClient ().GetByteArrayAsync (Annotation.House.Image.Url);
-				//		await imageCacheRepo.SaveHouseImages(new HouseImages(){ObjectId = Annotation.House.ObjectId, Image= byteArray});
-				//		image = UIImage.LoadFromData (NSData.FromArray (byteArray));
-
-				//	}
-				//	else
-				//	{
-				//		if (houseImageCache.Image != null)
-				//		{
-				//			image = UIImage.LoadFromData (NSData.FromArray (houseImageCache.Image));
-
-				//		}
-				//		else {
-				//			var byteArray = await new HttpClient ().GetByteArrayAsync (Annotation.House.Image.Url);
-				//			houseImageCache.Image = byteArray;
-				//			await imageCacheRepo.UpdateHouseImages(houseImageCache);
-				//			image = UIImage.LoadFromData (NSData.FromArray (byteArray));
-
-				//		}
-				//	}
-
-				//	this.houseImage.Image = image;
-
-				//} catch (Exception e) {
-				//	Console.WriteLine ("Error Retrieving Image. {0}", e.Message);
-				//} finally {
-				//	this.activityIndicator.StopAnimating ();
-				//	this.activityIndicator.Hidden = true;
-				//}
-			//}
+			
 		}
 
 		async partial void likeButton_TouchUpInside (UIButton sender)
